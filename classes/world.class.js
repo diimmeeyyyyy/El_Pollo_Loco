bc859eddd14592c37395ca00d346e03c48df9872
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-03-21 10:13:38 +0100

    implemented enemies (chicken) + their random position

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,20 +1,43 @@
 class World {
   gameCharacter = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-
+  canvas;
   ctx;
+
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+
     this.ctx.drawImage(
       this.gameCharacter.img,
       this.gameCharacter.x,
       this.gameCharacter.y,
       this.gameCharacter.width,
-      this.gameCharacter.height,
+      this.gameCharacter.height
     );
+
+    this.enemies.forEach(enemy => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+    });
+
+    // draw() wird immer wieder aufgerufen
+    self = this;
+    requestAnimationFrame(function(){
+      self.draw();
+    })
   }
+
+
 }
