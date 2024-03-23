a53ec2575bef58e29f30041aa837edf9187dd6f0
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-03-23 22:02:41 +0100

    added eventListener when keyup

--- javascript/game.js
+++ javascript/game.js
@@ -1,9 +1,56 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("Canvas");
   world = new World(canvas);
- 
-  /* console.log("My character is", world.gameCharacter) */
 }
+
+
+document.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+
+  if (event.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+
+  if (event.keyCode == 38) {
+    keyboard.UP = true;
+  }
+
+  if (event.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+
+  if (event.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
+});
+
+
+document.addEventListener("keyup", (event) => {
+  console.log(event);
+  if (event.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+
+  if (event.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+
+  if (event.keyCode == 38) {
+    keyboard.UP = false;
+  }
+
+  if (event.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+
+  if (event.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
+});
