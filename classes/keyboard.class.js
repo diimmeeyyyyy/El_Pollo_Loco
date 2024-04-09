84cae73c2779e0944b349f60ea63a54de158290b
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-04-09 20:33:50 +0200

    added mobileMovement arrows

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -5,4 +5,95 @@ class Keyboard {
   DOWN = false;
   SPACE = false;
   D = false;
+
+  constructor() {
+    this.bindKeyPressEvents();
+    /* this.bindButtonPressEvents(); */
+  }
+  
+  bindKeyPressEvents() {
+    document.addEventListener("keydown", (event) => {
+      /* console.log(event.keyCode); */
+      if (event.keyCode == 39) {
+        keyboard.RIGHT = true;
+      }
+
+      if (event.keyCode == 37) {
+        keyboard.LEFT = true;
+      }
+
+      if (event.keyCode == 38) {
+        keyboard.UP = true;
+      }
+
+      if (event.keyCode == 40) {
+        keyboard.DOWN = true;
+      }
+
+      if (event.keyCode == 32) {
+        keyboard.SPACE = true;
+      }
+
+      if (event.keyCode == 68) {
+        keyboard.D = true;
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      /*   console.log(event); */
+      if (event.keyCode == 39) {
+        keyboard.RIGHT = false;
+      }
+
+      if (event.keyCode == 37) {
+        keyboard.LEFT = false;
+      }
+
+      if (event.keyCode == 38) {
+        keyboard.UP = false;
+      }
+
+      if (event.keyCode == 40) {
+        keyboard.DOWN = false;
+      }
+
+      if (event.keyCode == 32) {
+        keyboard.SPACE = false;
+      }
+
+      if (event.keyCode == 68) {
+        keyboard.D = false;
+      }
+    });
+  } 
+
+ /*  bindButtonPressEvents() {
+    document
+      .getElementById("Walk_Left_Button")
+      .addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        this.LEFT = true;
+      });
+
+    document
+      .getElementById("Walk_Left_Button")
+      .addEventListener("touchend", (e) => {
+        e.preventDefault();
+        this.LEFT = false;
+      });
+
+    document
+      .getElementById("Walk_Right_Button")
+      .addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        this.RIGHT = true;
+      });
+
+    document
+      .getElementById("Walk_Right_Button")
+      .addEventListener("touchend", (e) => {
+        e.preventDefault();
+        this.RIGHT = false;
+      });
+  } */
 }
--- javascript/game.js
+++ javascript/game.js
@@ -28,65 +28,22 @@ function exitFullscreen() {
 }
 
 function startGame() {
-  document.getElementById("Start_Game_Button").style.display = "none";
+  removeButtons();
+  addMobileButtons();
   canvas = document.getElementById("Canvas");
   initLevel();
   world = new World(canvas, keyboard);
 }
 
-document.addEventListener("keydown", (event) => {
-  /* console.log(event.keyCode); */
-  if (event.keyCode == 39) {
-    keyboard.RIGHT = true;
-  }
+function addMobileButtons() {
+  document.getElementById("Mobile_Movement_Left").style.display = "flex";
+  document.getElementById("Mobile_Movement_Right").style.display = "flex";
+}
 
-  if (event.keyCode == 37) {
-    keyboard.LEFT = true;
-  }
-
-  if (event.keyCode == 38) {
-    keyboard.UP = true;
-  }
-
-  if (event.keyCode == 40) {
-    keyboard.DOWN = true;
-  }
-
-  if (event.keyCode == 32) {
-    keyboard.SPACE = true;
-  }
-
-  if (event.keyCode == 68) {
-    keyboard.D = true;
-  }
-});
-
-document.addEventListener("keyup", (event) => {
-  /*   console.log(event); */
-  if (event.keyCode == 39) {
-    keyboard.RIGHT = false;
-  }
-
-  if (event.keyCode == 37) {
-    keyboard.LEFT = false;
-  }
-
-  if (event.keyCode == 38) {
-    keyboard.UP = false;
-  }
-
-  if (event.keyCode == 40) {
-    keyboard.DOWN = false;
-  }
-
-  if (event.keyCode == 32) {
-    keyboard.SPACE = false;
-  }
-
-  if (event.keyCode == 68) {
-    keyboard.D = false;
-  }
-});
+function removeButtons() {
+  document.getElementById("Start_Game_Button").style.display = "none";
+  document.getElementById("Policy_Btn").style.display = "none";
+}
 
 function showKeyboardCommands() {
   let popUpBackground = document.createElement("div");
